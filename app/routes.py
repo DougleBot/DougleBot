fd261d341a3cab6d4b6812e89f5e84fcea23e923
Douglas Weiss <149013012+DougleBot@users.noreply.github.com>
2024-12-01 17:00:21 -0800

    PrelimStepStaging2
    Trying out some bells and whistles, please stand by

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def index():
-    return render_template("index.html", title="DougleBot")
+    return render_template("index.html", title="DougleBot", year=datetime.now().year)
